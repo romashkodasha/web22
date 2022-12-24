70f8c8e1970e953d625d952748f0138c542ce3b0
daria <daria@daria.huawei>
2022-12-24 12:00:37 +0300

    fix lab7

--- dance/permissions.py
+++ dance/permissions.py
@@ -1,11 +1,30 @@
-from rest_framework import permissions
+import logging
+
+import redis
+from django.conf import settings
+from rest_framework import permissions, status
+from rest_framework.response import Response
+
+from dance.models import User
+
+session_storage = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 
 class IsStaff(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and (request.user.is_staff or request.user.is_superuser))
+        session_id = request.COOKIES.get('session_id')
+        if session_id is None:
+            return False
+        user_id = session_storage.get(session_id)
+        user = User.objects.get(id=user_id)
+        return bool(user and (user.is_staff or user.is_superuser))
 
 
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_superuser)
+        session_id = request.COOKIES.get('session_id')
+        if session_id is None:
+            return False
+        user_id = session_storage.get(session_id)
+        user = User.objects.get(id=user_id)
+        return bool(user and user.is_superuser)
